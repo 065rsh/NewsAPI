c227d2a4342ec6ce5b591e5a83e621f00a5e850c
065rsh <gogl.arshdeep@gmail.com>
2020-10-21 21:29:43 +0530

    implemented the carousel

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,15 @@ import 'package:provider/provider.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget{
-  
-@override
-Widget build(BuildContext context){
-  return MaterialApp(
-    title: "News API",
-    home: 
-    ChangeNotifierProvider(
-      create: (context) => NewsArticleListViewModel(),
-      child: Homescreen()
-    ), 
-  );
-}
-
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "News API",
+      home: ChangeNotifierProvider(
+        create: (context) => NewsArticleListViewModel(),
+        child: Homescreen(),
+      ),
+    );
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,17 +14,58 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     final vm = Provider.of<NewsArticleListViewModel>(context);
+    print(vm.articles[0].title);
+
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-      title: SvgPicture.asset("assets/svg/NewsAPILogo.svg"),
-    ),
-    body: Card(
-
-    )
-
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: SvgPicture.asset("assets/svg/NewsAPILogo.svg"),
+      ),
+      body: Column(
+        children: [
+          CarouselSlider.builder(
+            options: CarouselOptions(height: 260),
+            itemCount: 15,
+            itemBuilder: (BuildContext context, int itemIndex) => Container(
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Column(
+                    children: [
+                      Container(
+                          child:
+                              Image.network(vm.articles[itemIndex].urlToImage)),
+                      Text(vm.articles[itemIndex].title),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: vm.articles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Column(
+                      children: [
+                        Container(
+                            child:
+                                Image.network(vm.articles[index].urlToImage)),
+                        Text(vm.articles[index].title),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
